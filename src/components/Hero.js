3b19089cca3d185b4c76d2c5a52b20d0f14ba8ec
codegeek777 <codegeek77@gmail.com>
2024-10-27 05:13:40 +0300

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,21 +9,30 @@ const Hero = () => {
           <div className="col-lg-6 col-md-6 col-12">
             <div className="hero-text">
               <h1 className="text-lighter">
-                <TypedText/>
+                <TypedText />
               </h1>
               <p className="line-height text-lighter">
-                Passionate about building dynamic and responsive websites.<br/>
+                Passionate about building dynamic and responsive websites.
+                <br />
                 focused on creating stunning, user-friendly websites and
-                applications.<br/> With expertise in HTML, CSS, and JavaScript, I
-                build responsive designs that deliver seamless experiences
-                across all devices.
+                applications.
+                <br /> With expertise in HTML, CSS, and JavaScript, I build
+                responsive designs that deliver seamless experiences across all
+                devices.
               </p>
-              <button className="button">Explore My Work </button>
+              <a
+                href="https://www.behance.net/asmaasaad7"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button"
+              >
+                Explore My Work{" "}
+              </a>
             </div>
           </div>
           <div className="col-lg-6 col-md-6 col-12">
             <div className="hero-img">
-                <img src={"/img/hero/header.gif"} alt="" />
+              <img src={"/img/hero/header.gif"} alt="" />
             </div>
           </div>
         </div>
